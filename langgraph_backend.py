274cb0b33014abfd928f862b35173fa62e2ff674
Mayank <imayank1724@gmail.com>
2026-04-16 16:19:02 +0530

    tools  integrated , can search on the internet

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,11 +1,14 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage,HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.tools import tool
 import sqlite3
-
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -25,14 +28,36 @@ def generate_title_llm(user_input):
     response = llm.invoke(prompt)
     return response.content.strip()
 
+#----------tools-------------------
 
+search_tool = DuckDuckGoSearchRun(region="us-en")
+
+
+@tool
+def get_stock_price(symbol: str) -> dict:
+    """
+    Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA') 
+    using Alpha Vantage with API key in the URL.
+    """
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=C9PE94QUEW9VWGFM"
+    r = requests.get(url)
+    return r.json()
+
+
+
+tools = [search_tool, get_stock_price]
+llm_with_tools = llm.bind_tools(tools)
+
+#--------------graph configuration----------------------
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
 def chat_node(state: ChatState):
     messages = state['messages']
-    response = llm.invoke(messages)
+    response = llm_with_tools.invoke(messages)
     return {"messages": [response]}
+
+tool_node=ToolNode(tools)
 
 conn=sqlite3.connect(database='chatbot.db',check_same_thread=False)
 
@@ -41,8 +66,12 @@ checkpointer=SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
+graph.add_node("tools", tool_node)
+
 graph.add_edge(START, "chat_node")
-graph.add_edge("chat_node", END)
+
+graph.add_conditional_edges("chat_node",tools_condition)
+graph.add_edge('tools', 'chat_node')
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
